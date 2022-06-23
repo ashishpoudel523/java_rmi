d5be19a7177f010ef980d3d4ffba832fa6a76113
Ashish Poudel <62178291+ashishpoudel523@users.noreply.github.com>
2022-06-23 21:31:20 +0545

    update UnicastRemoteObject

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+
 public class Server extends ImplExample { 
    public Server() {} 
    public static void main(String args[]) { 
